4aae3c3be21bf732f18bb7117da4804d32e7d010
Tawonga Chilongo <tmchilongo92@gmail.com>
2020-03-03 01:26:28 -0500

    Fresh start, new layout

---
+++ src/navigation.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+export default class Navigation extends Component {
+    render() {
+        return (
+            <div className="Navigation-wrapper">
+                <div className="left-column">
+
+                </div>
+
+                <div className="middle-column">
+
+                </div>
+
+                <div className="right-column">
+                    
+                </div>
+            </div>
+        )
+    }
+}
